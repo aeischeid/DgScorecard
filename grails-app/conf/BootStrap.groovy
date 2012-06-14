a9bd9706f0e519ccdda4257514e7a76bd9eafb03
Craig Atkinson <atk.craig@gmail.com>
2012-06-13 20:22:56 -0500

    Only load default data in an empty db

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -6,16 +6,20 @@ import grails.converters.JSON
 class BootStrap {
 
     def init = { servletContext ->
-        User user1 = new User(username: "user1", password: "pass1", email: "user1@test.com", enabled: true).save()
-        User user2 = new User(username: "user2", password: "pass1", email: "user2@test.com", enabled: true).save()
+        boolean emptyDatabase = !User.count()
 
-        Course course1 = new Course(name: "Course 1", holes: 18, par: 54).save(failOnError: true)
+        if (emptyDatabase) {
+            User user1 = new User(username: "user1", password: "pass1", email: "user1@test.com", enabled: true).save()
+            User user2 = new User(username: "user2", password: "pass1", email: "user2@test.com", enabled: true).save()
 
-        Score user1Course1Score = new Score(score: 55, course: course1, player: user1).save(failOnError: true)
-        Score user1Course1InProgressScore = new Score(score: 56, course: course1, player: user1, inProgress: true, notes: "In progress score").save(failOnError: true)
-        Score user1Course1FinishedScore = new Score(score: 56, course: course1, player: user1, inProgress: false, notes: "Finished score").save(failOnError: true)
+            Course course1 = new Course(name: "Course 1", holes: 18, par: 54).save(failOnError: true)
 
-        Score user2Course1Score = new Score(score: 56, course: course1, player: user2, notes: "user2Course1Score").save(failOnError: true)
+            Score user1Course1Score = new Score(score: 55, course: course1, player: user1).save(failOnError: true)
+            Score user1Course1InProgressScore = new Score(score: 56, course: course1, player: user1, inProgress: true, notes: "In progress score").save(failOnError: true)
+            Score user1Course1FinishedScore = new Score(score: 56, course: course1, player: user1, inProgress: false, notes: "Finished score").save(failOnError: true)
+
+            Score user2Course1Score = new Score(score: 56, course: course1, player: user2, notes: "user2Course1Score").save(failOnError: true)
+        }
 
         JSON.registerObjectMarshaller(User) { User user ->
             return [
